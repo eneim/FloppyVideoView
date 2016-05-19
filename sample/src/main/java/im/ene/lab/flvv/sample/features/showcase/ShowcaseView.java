7cb2efbcbff9c9e509041ac41afb75d82538485a
Nguyen Hoai Nam <mr.nguyenhoainam@gmail.com>
2016-05-19 16:07:06 +0900

    Update dependencies

--- sample/src/main/java/im/ene/lab/flvv/sample/features/showcase/ShowcaseView.java
+++ sample/src/main/java/im/ene/lab/flvv/sample/features/showcase/ShowcaseView.java
@@ -16,7 +16,13 @@
 
 package im.ene.lab.flvv.sample.features.showcase;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
 import im.ene.lab.flvv.sample.model.Model;
+import im.ene.lab.toro.ToroAdapter;
+import im.ene.lab.toro.ToroViewHolder;
 import java.util.List;
 
 /**
@@ -28,4 +34,70 @@ interface ShowcaseView {
   void showLoading(boolean isLoading);
 
   void updateData(boolean forceUpdate, List<? extends Model> items);
+
+  class Adapter extends ToroAdapter<VideoViewHolder> {
+
+    @Nullable @Override protected Object getItem(int position) {
+      return null;
+    }
+
+    @Override public VideoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      return null;
+    }
+
+    @Override public int getItemCount() {
+      return 0;
+    }
+  }
+
+  class VideoViewHolder extends ToroViewHolder {
+
+    public VideoViewHolder(View itemView) {
+      super(itemView);
+    }
+
+    @Override public void bind(@Nullable Object object) {
+
+    }
+
+    @Override public boolean wantsToPlay() {
+      return false;
+    }
+
+    @Override public boolean isAbleToPlay() {
+      return false;
+    }
+
+    @Nullable @Override public String getVideoId() {
+      return null;
+    }
+
+    @NonNull @Override public View getVideoView() {
+      return null;
+    }
+
+    @Override public void start() {
+
+    }
+
+    @Override public void pause() {
+
+    }
+
+    @Override public int getDuration() {
+      return 0;
+    }
+
+    @Override public int getCurrentPosition() {
+      return 0;
+    }
+
+    @Override public void seekTo(int pos) {
+
+    }
+
+    @Override public boolean isPlaying() {
+      return false;
+    }
+  }
 }
